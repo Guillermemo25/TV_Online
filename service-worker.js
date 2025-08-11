ac3bce101e0204a349097dbb347a4fb0a3abb118
Guillermo Cahuich <jcahuich@marketshareit.mx>
2025-08-11 02:55:16 -0600

    Se añade funcionalidad que permite instalación como aplicación

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'canales-vivo-cache-v1';
+const urlsToCache = [
+  '.',
+  'v1.html',
+  'manifest.json',
+  'icons/icon-192x192.png',
+  'icons/icon-512x512.png',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js'
+  // Agrega aquí otros recursos estáticos que uses
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
